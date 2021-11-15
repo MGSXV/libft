c6edccceee9f26e7f6a751242adb3b9c3dd9c579
Soufiane Elkhamlichi <42505085+05soufiane@users.noreply.github.com>
2021-11-15 21:56:14 +0100

    Create ft_atoi.c
    ft_atoi is is a re-writing of the predefined function "atoi" in C.

---
+++ ft_atoi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 10:24:18 by sel-kham          #+#    #+#             */
+/*   Updated: 2021/11/12 01:42:29 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+static int	ft_isspace(int c)
+{
+	int	res;
+
+	res = 0;
+	if (c == '\f' || c == '\n' || c == '\r'
+		|| c == '\t' || c == '\v' || c == ' ')
+		res = 1;
+	return (res);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	i;
+	int	sign;
+
+	result = 0;
+	i = 0;
+	sign = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		result = result * 10 + (int) str[i] - 48;
+		i++;
+	}
+	return (result * sign);
+}

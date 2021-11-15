71af043094b08c014e1f14638e74e7e2ce9bbe45
Soufiane Elkhamlichi <42505085+05soufiane@users.noreply.github.com>
2021-11-15 22:40:28 +0100

    Create ft_strjoin
    ft_strjoin returns a new string, which is the result of the concatenation of ’s1’ and ’s2’.

---
+++ ft_strjoin.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 14:15:02 by sel-kham          #+#    #+#             */
+/*   Updated: 2021/11/12 01:59:34 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	i;
+
+	s1_len = ft_strlen(s1);
+	i = -1;
+	s2_len = ft_strlen(s2);
+	result = (char *) malloc((s1_len + s2_len) * sizeof(char));
+	if (!result)
+		return (NULL);
+	result = ft_memcpy(result, s1, s1_len);
+	while (++i < s2_len)
+		result[i + s1_len] = s2[i];
+	result[s1_len + s2_len] = '\0';
+	return (result);
+}

1581e9095fefb1d36159f412c2f2c526ee63e370
Soufiane Elkhamlichi <42505085+05soufiane@users.noreply.github.com>
2021-11-15 22:07:38 +0100

    Create ft_memcmp.c
    ft_memcmp is a re-writing of the predefined function "memcmp" in C.

---
+++ ft_memcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sel-kham <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 19:58:07 by sel-kham          #+#    #+#             */
+/*   Updated: 2021/11/12 01:51:24 by sel-kham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
+
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
+	i = -1;
+	while (++i < n)
+		if (str1[i] - str2[i])
+			return (str1[i] - str2[i]);
+	return (0);
+}

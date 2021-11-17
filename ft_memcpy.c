5591641fff4a7ae13ec66b05903805ad6fb100e7
Soufiane Elkhamlichi <42505085+05soufiane@users.noreply.github.com>
2021-11-17 14:40:49 +0100

    Update ft_memcpy.c
    * Updating the third parameter's type to "size_t" rather than "unsigned long".
    * Protecting the function with a condition to avoid the crush

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -9,17 +9,19 @@
 /*   Updated: 2021/11/12 01:51:59 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-void	*ft_memcpy(void *dst, const void *src, unsigned long n)
+#include "libft.h"
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned long	i;
+	size_t	i;
 	char			*dst2;
 	char			*src2;
 
 	i = -1;
+	if (!dst && !src)
+		return (NULL);
 	dst2 = (char *) dst;
 	src2 = (char *) src;
 	while (++i < n)
-		dst2[i] = ((char *) src)[i];
+		dst2[i] = src2[i];
 	return (dst);
 }

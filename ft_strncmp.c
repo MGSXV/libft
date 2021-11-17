bd99cdfef1db8eaa4d22e8985dd7f87d0befe1d5
Soufiane Elkhamlichi <42505085+05soufiane@users.noreply.github.com>
2021-11-17 12:46:12 +0100

    Update ft_strncmp.c
    - Managing non-ASCII characters.
    - Fixing some bugs in the algorithm, to managing the overflow and some critical cases.

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,12 +14,16 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 0;
-	while ((s1[i] && s2[i]) || i < n)
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
+	while ((str1[i] || str2[i]) && i < n)
 	{
-		if (s1[i] - s2[i])
-			return (s1[i] - s2[i]);
+		if (str1[i] - str2[i])
+			return (str1[i] - str2[i]);
 		i++;
 	}
 	return (0);

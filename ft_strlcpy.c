bf32e73ef1f23f35f99afd5ff0b0eb0c2dd275bb
Soufiane Elkhamlichi <42505085+05soufiane@users.noreply.github.com>
2021-11-17 16:37:48 +0100

    Update ft_strlcpy.c
    Updating the algorithm by using "ft_memcpy" function

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -15,20 +15,13 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t	s_len;
 	size_t	i;
-	char	*coppied;
 
-	coppied = dst;
-	s_len = ft_strlen(src);
 	i = 0;
-	if (!dstsize)
-		return (s_len);
-	while (i < s_len && i < dstsize)
-	{
-		coppied[i] = src[i];
-		i++;
-	}
-	coppied[i] = '\0';
-	while (++i < dstsize - 1)
-		coppied[i] = '\0';
-	return (s_len);
+	s_len = ft_strlen(src);
+	if (s_len < dstsize)
+		ft_memcpy(dst, src, dstsize);
+	else
+		ft_memcpy(dst, src, s_len);
+	dst[i] = '\0';
+	return (dstsize);
 }

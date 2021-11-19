bf7802f9f375a4a385d11e89d26b71ff997678f6
Soufiane Elkhamlichi <42505085+05soufiane@users.noreply.github.com>
2021-11-19 06:21:50 +0100

    Update ft_strchr.c
    Casting "c" to char and working on norms.

--- ft_strchr.c
+++ ft_strchr.c
@@ -13,14 +13,13 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-
 	int		i;
 
 	i = -1;
 	while (s[++i])
-		if (s[i] == c)
-			return (char *) s+i;
+		if (s[i] == (char) c)
+			return ((char *) s + i);
 	if (!c)
-		return (char *) s+i;
+		return ((char *) s + i);
 	return (0);
 }

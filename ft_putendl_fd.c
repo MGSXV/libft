b5a5e0780964d0f24cbd3567ffa7272138d0abd1
05soufiane <05soufiane@gmail.com>
2021-11-19 21:45:14 +0100

    Securing the function

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -13,6 +13,8 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
+	if (!s)
+		return ;
 	write(fd, s, ft_strlen(s));
 	write(fd, "\n", 1);
 }
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -14,5 +14,7 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
+	if (!s)
+		return ;
 	write(fd, s, ft_strlen(s));
 }

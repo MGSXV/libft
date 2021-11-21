3c22950d2a481ba852d90bb7033d7b86e15a76cd
05soufiane <05soufiane@gmail.com>
2021-11-21 17:47:25 +0100

    Re-writing the algorithm

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -17,21 +17,20 @@ size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 	size_t	d_len;
 	size_t	s_len;
 	size_t	i;
-	char	*concatenated;
+	size_t	d;
 
 	d_len = ft_strlen(dest);
 	s_len = ft_strlen(src);
-	concatenated = dest;
+	d = d_len;
 	i = 0;
-	if (dstsize < d_len || dstsize == 0)
-		return (dstsize + d_len);
-	while (i < dstsize - d_len - 1 && i < s_len)
+	if (dstsize == 0 || dstsize <= d_len)
+		return (dstsize + s_len);
+	while (d < dstsize - 1 && src[i])
 	{
-		concatenated[d_len + i] = src[i];
+		dest[d] = src[i];
 		i++;
+		d++;
 	}
-	i = d_len + s_len - 2;
-	while (++i < dstsize - d_len - s_len - 1)
-		concatenated[d_len + i] = '\0';
+	dest[d] = '\0';
 	return (d_len + s_len);
 }

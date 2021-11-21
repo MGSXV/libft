d494254f3b7eac8e33f6e8f53518c42418ff61fb
05soufiane <05soufiane@gmail.com>
2021-11-21 23:14:33 +0100

    Creating the function ft_lstnew

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -11,3 +11,14 @@
 /* ************************************************************************** */
 #include "libft.h"
 
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_element;
+
+	new_element = (t_list *) malloc(sizeof(t_list));
+	if (!new_element)
+		return (NULL);
+	new_element->content = content;
+	new_element->next = NULL;
+	return (new_element);
+}

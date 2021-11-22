c14017a052ff176026fbd5bba7399b9754a67d74
05soufiane <05soufiane@gmail.com>
2021-11-22 05:40:22 +0100

    Create "ft_lstadd_front" function

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -11,3 +11,11 @@
 /* ************************************************************************** */
 #include "libft.h"
 
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!(*lst))
+		*lst = new;
+	else
+		new->next = *lst;
+		*lst = new;
+}

516b1714d912bea8dfeb1856189a5dcb11cf4d60
05soufiane <05soufiane@gmail.com>
2021-11-23 22:47:08 +0100

    Updating memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: sel-kham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 08:21:27 by sel-kham          #+#    #+#             */
-/*   Updated: 2021/11/12 01:51:59 by sel-kham         ###   ########.fr       */
+/*   Updated: 2021/11/23 21:37:36 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -19,6 +19,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	if (!dst && !src)
 		return (NULL);
 	while (++i < n)
-		*((char *) (dst + i)) = *((char *) src + i);
+		*((char *)(dst + i)) = *((char *)(src + i));
 	return (dst);
 }
